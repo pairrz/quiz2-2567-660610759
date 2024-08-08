8eaffb429868841fca6a4e1c47aa70a3d7377cf0
pairrz <dararat.buayim@gmail.com>
2024-08-08 15:38:35 +0700

    03

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -16,6 +16,10 @@ export default function Comment({
       <div className="d-flex align-items-center p-3">
         <img src={ImagePath} width="90px" className="rounded-circle me-4"></img>
         <span className="text-center display-6 me-auto">{username}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{likes}</span>
+            </div>
         </div>
     </div>
   );
